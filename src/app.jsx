57a1f535373e925470b55e32c69d6b6475090bf5
savalaio <savalaio15@hotmail.com>
2026-03-13 09:19:09 -0300

    Remove Sidebar and Dashboard components

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,7 @@
-import Sidebar from "./components/Sidebar.jsx";
-import Dashboard from "./pages/Dashboard.jsx";
-
 export default function App() {
   return (
-    <div className="layout">
-      <Sidebar />
-      <Dashboard />
+    <div style={{color:"white"}}>
+      Painel GPS funcionando 🚀
     </div>
-  );
+  )
 }
