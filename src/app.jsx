ba4735fe23395bfcfca6e4152fde448a066f13c3
savalaio <savalaio15@hotmail.com>
2026-03-13 10:38:55 -0300

    Add padding and font size to App component

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,6 @@
 export default function App() {
   return (
-    <div style={{color:"white"}}>
+    <div style={{color:"white", padding:"40px", fontSize:"30px"}}>
       Painel GPS funcionando 🚀
     </div>
   )
